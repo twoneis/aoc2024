a051d86a30b5649926e331e6c4206b1281b06933
twoneis <sanjay29@sapura.de>
2024-12-02 14:29:06 +0100

    day 2 part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -6,9 +6,9 @@ fn main() {
     let lines = content.split("\n");
 
     let mut res = 0;
+    let mut res2 = 0;
 
     for line in lines {
-        let mut order = true;
         let parts: Vec<u64> = line
             .split_whitespace()
             .map(|x| x.parse::<u64>().expect("Parse error"))
@@ -17,26 +17,47 @@ fn main() {
             continue;
         }
 
-        if parts[0] <= parts[1] {
-            for i in 1..parts.len() {
-                if parts[i - 1] >= parts[i] || parts[i - 1] + 3 < parts[i] {
-                    order = false;
-                    break;
-                }
-            }
-        } else {
-            for i in 1..parts.len() {
-                if parts[i - 1] <= parts[i] || parts[i - 1] > parts[i] + 3 {
-                    order = false;
-                    break;
-                }
-            }
+        if sorted(&parts) {
+            res += 1;
         }
 
-        if order {
-            res += 1;
+        if semi_sorted(&parts) {
+            res2 += 1;
         }
     }
 
     println!("Part 1: {res}");
+    println!("Part 1: {res2}");
+}
+
+fn sorted(parts: &Vec<u64>) -> bool {
+    if parts[0] <= parts[1] {
+        for i in 1..parts.len() {
+            if parts[i - 1] >= parts[i] || parts[i - 1] + 3 < parts[i] {
+                return false;
+            }
+        }
+    } else {
+        for i in 1..parts.len() {
+            if parts[i - 1] <= parts[i] || parts[i - 1] > parts[i] + 3 {
+                return false;
+            }
+        }
+    }
+
+    true
+}
+fn semi_sorted(parts: &Vec<u64>) -> bool {
+    if sorted(parts) {
+        true
+    } else {
+        for i in 0..parts.len() {
+            let mut cpy = parts.clone();
+            cpy.remove(i);
+            if sorted(&cpy) {
+                return true;
+            }
+        }
+        false
+    }
 }
