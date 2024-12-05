21394866d3fa3899001c2e5eb8bf5b600fd117c0
twoneis <sanjay29@sapura.de>
2024-12-05 21:31:13 +0100

    day 5 part 2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,4 +1,5 @@
 use multimap::MultiMap;
+use std::collections::HashMap;
 use std::fs;
 
 fn main() {
@@ -22,7 +23,7 @@ fn main() {
         .map(|line| (line[0], line[1]))
         .collect();
 
-    let res: u32 = lines
+    let res1: u32 = lines
         .iter()
         .map(|line| {
             line.split(",")
@@ -51,5 +52,68 @@ fn main() {
         .map(|line| line[line.len() / 2])
         .sum();
 
-    println!("{res}");
+    let res2: u32 = lines
+        .iter()
+        .map(|line| {
+            line.split(",")
+                .filter_map(|s| match s.parse::<u32>() {
+                    Ok(r) => Some(r),
+                    Err(_) => None,
+                })
+                .collect::<Vec<u32>>()
+        })
+        .filter_map(|line| {
+            let mut swapped;
+            let mut some_swap = false;
+            let mut arr: Vec<u32> = line.clone();
+            for i in 0..arr.len() {
+                swapped = false;
+                for j in 0..line.len() - i - 1 {
+                    match rules.get_vec(&arr[j + 1]) {
+                        Some(r) => {
+                            if r.contains(&arr[j]) {
+                                arr.swap(j, j + 1);
+                                swapped = true;
+                                some_swap = true;
+                            }
+                        }
+                        None => continue,
+                    }
+                }
+
+                if !swapped {
+                    break;
+                }
+            }
+            if some_swap {
+                Some(arr)
+            } else {
+                None
+            }
+        })
+        .filter(|line| {
+            for i in 0..line.len() {
+                match rules.get_vec(&line[i]) {
+                    Some(r) => {
+                        for j in 0..i {
+                            if r.contains(&line[j]) {
+                                return false;
+                            }
+                        }
+                    }
+                    None => continue,
+                }
+            }
+            true
+        })
+        .map(|line| line[line.len() / 2])
+        .sum();
+    // .filter(|line| line.len() != 0)
+    // .collect();
+
+    println!("Part 1: {res1}");
+    println!("Part 2: {res2}");
+    // for line in res2 {
+    //     println!("{:?}", line);
+    // }
 }
