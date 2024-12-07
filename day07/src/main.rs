5c3302c82ce8c1b50a6ab13c5bdb059227840998
twoneis <sanjay29@sapura.de>
2024-12-07 11:24:36 +0100

    day 7 part 2

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -17,7 +17,7 @@ fn main() {
         .collect();
 
     let res: u64 = lines
-        .into_iter()
+        .iter()
         .filter_map(|line| {
             if solvable(line[0], line[1], &line[2..]) {
                 Some(line[0])
@@ -27,7 +27,19 @@ fn main() {
         })
         .sum();
 
+    let res2: u64 = lines
+        .iter()
+        .filter_map(|line| {
+            if solvable2(line[0], line[1], &line[2..]) {
+                Some(line[0])
+            } else {
+                None
+            }
+        })
+        .sum();
+
     println!("Part 1: {res}");
+    println!("Part 2: {res2}");
 }
 
 fn solvable(target: u64, so_far: u64, remaining: &[u64]) -> bool {
@@ -36,5 +48,21 @@ fn solvable(target: u64, so_far: u64, remaining: &[u64]) -> bool {
     } else {
         solvable(target, so_far + remaining[0], &remaining[1..])
             || solvable(target, so_far * remaining[0], &remaining[1..])
+    }
+}
+
+fn solvable2(target: u64, so_far: u64, remaining: &[u64]) -> bool {
+    if remaining.len() == 0 {
+        target == so_far
+    } else {
+        solvable2(target, so_far + remaining[0], &remaining[1..])
+            || solvable2(target, so_far * remaining[0], &remaining[1..])
+            || solvable2(
+                target,
+                (so_far.to_string() + &remaining[0].to_string())
+                    .parse()
+                    .expect("Concat error"),
+                &remaining[1..],
+            )
     }
 }
