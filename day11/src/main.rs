fe79459424cbd2e15c19d8abbed8aab912227636
twoneis <sanjay29@sapura.de>
2024-12-22 01:56:58 +0100

    day 11 part 2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,48 +1,61 @@
+use std::collections::HashMap;
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
-    if args.len() != 2 {
-        println!("Usage: cargo run <input_file>");
+    if args.len() != 3 {
+        println!("Usage: cargo run <input_file> <n_iterations>");
         return;
     }
 
-    let mut stones: Vec<String> = std::fs::read_to_string(&args[1])
+    let iterations: usize = args[2]
+        .parse::<usize>()
+        .expect("Usage: cargo run <input_file> <n_iterations>");
+
+    let stones: Vec<String> = std::fs::read_to_string(&args[1])
         .expect("Error reading file")
         .lines()
         .map(|s| s.split_whitespace().map(String::from))
         .flatten()
         .collect();
+    let len = stones.len();
 
-    for _ in 0..25 {
-        // println!("{:?}", stones);
-        let mut i = 0;
-        while i < stones.len() {
-            match stones[i].as_ref() {
-                "0" => stones[i] = "1".to_string(),
-                _ if stones[i].len() % 2 == 0 => {
-                    let lower: String = stones[i][..stones[i].len() / 2]
-                        .parse::<usize>()
-                        .expect("Parsing error")
-                        .to_string();
-                    let upper: String = stones[i][stones[i].len() / 2..]
-                        .parse::<usize>()
-                        .expect("Parsing error")
-                        .to_string();
-                    stones.remove(i);
-                    stones.insert(i, lower);
-                    i += 1;
-                    stones.insert(i, upper);
-                }
-                _ => {
-                    let new: String =
-                        (stones[i].parse::<usize>().expect("Parsing error") * 2024).to_string();
-                    stones.remove(i);
-                    stones.insert(i, new);
-                }
+    let res: usize = (0..iterations)
+        .fold(
+            HashMap::from_iter(stones.into_iter().zip(vec![1; len])),
+            |acc, _| step(&acc),
+        )
+        .values()
+        .sum();
+
+    println!("After {} iterations: {} stones", iterations, res);
+}
+
+fn step(stones: &HashMap<String, usize>) -> HashMap<String, usize> {
+    stones.iter().fold(HashMap::new(), |mut acc, (stone, num)| {
+        match stone.as_str() {
+            "0" => {
+                *acc.entry("1".to_string()).or_insert(0) += num;
+                acc
             }
-            i += 1;
+            s if s.len() % 2 == 0 => {
+                let lower: String = s[..s.len() / 2]
+                    .parse::<usize>()
+                    .expect("Parsing error")
+                    .to_string();
+                let upper: String = s[s.len() / 2..]
+                    .parse::<usize>()
+                    .expect("Parsing error")
+                    .to_string();
+                *acc.entry(lower.to_string()).or_insert(0) += num;
+                *acc.entry(upper.to_string()).or_insert(0) += num;
+                acc
+            }
+            s => {
+                *acc.entry((s.parse::<usize>().expect("Parsing error") * 2024).to_string())
+                    .or_insert(0) += num;
+                acc
+            }
         }
-    }
-
-    println!("Part 1: {}", stones.len());
+    })
 }
